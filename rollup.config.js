d70ecf8319f937ebfecb1aae86901ae68cf353ee
psirenny <djtorres0@gmail.com>
2018-09-23 17:06:04 -0400

    feat: bundle for es as well as cjs

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,10 @@ import rollupPluginBabel from 'rollup-plugin-babel';
 import rollupPluginFlowEntry from 'rollup-plugin-flow-entry';
 
 export default {
-  output: {
-    format: ['cjs'],
-  },
+  output: [
+    { file: 'dist/cjs/index.js', format: 'cjs' },
+    { file: 'dist/es/index.js', format: 'es' },
+  ],
   plugins: [
     rollupPluginFlowEntry(),
     rollupPluginBabel(),
